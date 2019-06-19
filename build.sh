34c722e6154749256e32966e32fa15f794bcacfb
dennis <anzzyolivia@gmail.com>
2019-06-19 16:23:34 +0800

    v1.3.6.19

--- build.sh
+++ build.sh
@@ -1,13 +1,31 @@
 #!/bin/sh
 echo "====================================================================="
-echo "[信息]Docker环境OpenResty+PHP7一键部署 By anzzy 适用于Ubuntu 16.04"
+echo "[信息]Docker环境OpenResty+PHP7+Swoole一键部署 By anzzy 适用于Ubuntu 16.04"
 echo "[信息]最后更新：2019-06-19"
 echo "[信息]OpenResty版本：1.15.8.1"
 echo "[信息]PHP版本：7.3.6"
+echo "[信息]Swoole版本：4.3.5"
 echo "====================================================================="
+
+echo "[警告]请在全新环境中执行该脚本，是否继续？(y/n)"
+stty erase '^H' && read -p "(默认: n):" start_build
+if [ ${start_build} != "y" ] ; then
+    exit
+fi
+
+echo "是否安装Swoole扩展？(y/n)"
+stty erase '^H' && read -p "(默认: n):" install_swoole
+
+#echo "是否安装cURL扩展？(y/n)"
+#stty erase '^H' && read -p "(默认: n):" install_curl
+
+#echo "是否安装mysqli扩展？(y/n)"
+#stty erase '^H' && read -p "(默认: n):" install_mysqli
 
 cd /opt
 apt-get update
+
+echo "[信息]正在准备依赖项"
 apt-get install -y wget
 apt-get install -y make
 apt-get install -y gcc
@@ -20,6 +38,10 @@ apt-get install -y curl
 apt-get install -y redis-server
 apt-get install -y vim
 apt-get install -y libxml2-dev
+apt-get install -y autoconf
+
+#echo "[信息]安装常用工具"
+#apt-get install -y net-tools
 
 echo "[信息]开始下载OpenResty..."
 wget https://openresty.org/download/openresty-1.15.8.1.tar.gz
@@ -45,7 +67,7 @@ cd /usr/local/etc/
 wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/php-fpm.conf
 chmod 777 php-fpm.conf
 
-echo "[信息]创建php.ini"
+echo "[信息]创建php.ini(生产环境)"
 cp /opt/php-7.3.6/php.ini-production /usr/local/lib/
 mv /usr/local/lib/php.ini-production /usr/local/lib/php.ini
 
@@ -56,27 +78,44 @@ echo "[信息]正在处理OpenResty nginx.conf"
 cd /usr/local/openresty/nginx/conf/
 wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/nginx.conf -O nginx.conf
 mkdir /opt/www
-echo "[信息]已将默认目录改为/opt/www/"
+echo "[信息]已将网站默认目录改为 /opt/www/"
 
 echo "[信息]创建测试文件..."
+cd /opt/www
 echo "OpenResty Running..." > index.html
 echo "<?php echo 'PHP Working';?>" > index.php
 
 echo "[信息]正在处理Redis配置..."
 cd /etc/redis/
 wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/redis.conf -O redis.conf
+service redis-server restart
+
+if [ ${install_swoole} = "y" ] ; then
+    echo "[信息]开始安装Swoole扩展..."
+    cd /opt
+    wget https://github.com/swoole/swoole-src/archive/v4.3.5.tar.gz
+    tar -xvf v4.3.5.tar.gz
+    cd swoole-src-4.3.5
+    phpize
+    ./configure
+    make && make install
+
+    echo "#Added by build.sh (https://github.com/anzzyd/docker-automake)" >> /usr/local/lib/php.ini
+    echo "extension=swoole" >> /usr/local/lib/php.ini
+fi
+
+echo "[信息]正在启动PHP7..."
+/usr/local/sbin/php-fpm
 
 echo "[信息]正在启动OpenResty..."
 /usr/local/openresty/bin/openresty -s stop
 /usr/local/openresty/bin/openresty
-echo "[信息]正在启动PHP7..."
-/usr/local/sbin/php-fpm
 
 echo "[信息]测试静态页结果:"
 curl http://127.0.0.1
 echo "[信息]测试PHP页结果:"
 curl http://127.0.0.1/index.php
 
-echo "[信息]OpenResty+PHP7部署已完成"
+echo "[信息]所有项目均部署完成"
 echo "[信息]网站默认目录为：/opt/www/"
-echo "[信息]Redis端口为：16379"
+echo "[信息]Redis端口为：16379(已开启UNIX Socket)"
