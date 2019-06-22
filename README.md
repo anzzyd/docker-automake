43bbd12473c0f892cfeed8993b40de4e2bfa4565
dennis <anzzyolivia@gmail.com>
2019-06-22 17:39:26 +0800

    v1.5.6.22

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-# Docker 一键部署OpenResty、PHP、Redis、Swoole
+# Docker 一键部署OpenResty、PHP、Redis Server
 #### 安装命令
 `apt-get update;apt-get install -y wget;wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/build.sh;chmod 777 build.sh;./build.sh`
 
 ##### 运行环境：`Ubuntu 16.04`
 - OpenResty版本：`1.15.8.1`
 - PHP版本：`7.3.6`
-- Swoole版本：`4.3.5`
+------------
+PHP包含的扩展
+- Swoole( v4.3.5 )
+- Redis( v4.3.0 )
+- cURL
+- MySQL
 
 ##### 请使用全新环境安装
-- 网站默认目录：`/opt/www/`
+- 网站默认根目录：`/opt/www/`
 - Redis端口：`16379`
--- build.sh
+++ build.sh
@@ -1,50 +1,48 @@
 #!/bin/sh
 echo "====================================================================="
-echo "[信息]Docker环境OpenResty+PHP7+Swoole一键部署 By anzzy 适用于Ubuntu 16.04"
+echo "[信息]Docker环境OpenResty+PHP7+Swoole+Redis一键部署 By anzzy 适用于Ubuntu 16.04"
 echo "[信息]最后更新：2019-06-19"
 echo "[信息]OpenResty版本：1.15.8.1"
 echo "[信息]PHP版本：7.3.6"
 echo "[信息]Swoole版本：4.3.5"
+echo "[信息]Redis版本：4.3.0 stable"
 echo "====================================================================="
 
-echo "[警告]请在全新环境中执行该脚本，是否继续？(y/n)"
+echo "[警告]请确保在全新环境中执行该脚本，是否继续？(y/n)"
 stty erase '^H' && read -p "(默认: n):" start_build
 if [ ${start_build} != "y" ] ; then
     exit
 fi
 
+echo "是否跳过OpenResty、PHP的下载过程？(y\n)"
+stty erase '^H' && read -p "(默认: n):" skip_wget_download
+
 echo "是否安装Swoole扩展？(y/n)"
 stty erase '^H' && read -p "(默认: n):" install_swoole
 
-#echo "是否安装cURL扩展？(y/n)"
-#stty erase '^H' && read -p "(默认: n):" install_curl
+echo "是否安装Redis扩展？(y/n)"
+stty erase '^H' && read -p "(默认: n):" install_redis_extension
 
-#echo "是否安装mysqli扩展？(y/n)"
-#stty erase '^H' && read -p "(默认: n):" install_mysqli
+echo "是否安装Redis Server？(y/n)"
+stty erase '^H' && read -p "(默认: n):" install_redis
+
+echo "是否开启Zend OPcache？(y/n)"
+stty erase '^H' && read -p "(默认: n):" load_opcache
 
 cd /opt
 apt-get update
 
 echo "[信息]正在准备依赖项"
-apt-get install -y wget
-apt-get install -y make
-apt-get install -y gcc
-apt-get install -y libpcre3-dev
-apt-get install -y libssl-dev
-apt-get install -y perl
-apt-get install -y build-essential
-apt-get install -y libpcre3 libpcre3-dev zlib1g zlib1g-dev libssl-dev
-apt-get install -y curl
-apt-get install -y redis-server
-apt-get install -y vim
-apt-get install -y libxml2-dev
-apt-get install -y autoconf
+apt-get install -y wget make gcc libpcre3-dev libssl-dev perl build-essential libpcre3 zlib1g zlib1g-dev libssl-dev curl vim libxml2-dev autoconf libcurl4-gnutls-dev
 
 #echo "[信息]安装常用工具"
 #apt-get install -y net-tools
 
-echo "[信息]开始下载OpenResty..."
-wget https://openresty.org/download/openresty-1.15.8.1.tar.gz
+if [ ${skip_wget_download} != "y" ] ; then
+    echo "[信息]开始下载OpenResty..."
+    wget https://openresty.org/download/openresty-1.15.8.1.tar.gz
+fi
+
 tar -xvf openresty-1.15.8.1.tar.gz
 cd openresty-1.15.8.1
 echo "[信息]开始编译OpenResty..."
@@ -53,19 +51,22 @@ make && make install
 
 cd ..
 
-echo "[信息]开始下载PHP7.3.6..."
-wget https://www.php.net/distributions/php-7.3.6.tar.gz
+if [ ${skip_wget_download} != "y" ] ; then
+    echo "[信息]开始下载PHP7.3.6..."
+    wget https://www.php.net/distributions/php-7.3.6.tar.gz
+fi
+
 tar -xvf php-7.3.6.tar.gz
 cd php-7.3.6
 echo "[信息]开始编译PHP7.3.6..."
-./configure --enable-fpm --with-fpm-user=nginx --with-fpm-group=nginx
+./configure --enable-fpm --with-curl --with-mysqli=mysqlnd --with-pdo-mysql=mysqlnd --with-fpm-user=nginx --with-fpm-group=nginx
 make && make install
 
 echo "[信息]正在处理PHP环境..."
 mv /usr/local/etc/php-fpm.d/www.conf.default /usr/local/etc/php-fpm.d/www.conf
 cd /usr/local/etc/
 wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/php-fpm.conf
-chmod 777 php-fpm.conf
+#chmod 777 php-fpm.conf
 
 echo "[信息]创建php.ini(生产环境)"
 cp /opt/php-7.3.6/php.ini-production /usr/local/lib/
@@ -83,12 +84,16 @@ echo "[信息]已将网站默认目录改为 /opt/www/"
 echo "[信息]创建测试文件..."
 cd /opt/www
 echo "OpenResty Running..." > index.html
-echo "<?php echo 'PHP Working';?>" > index.php
+echo "<?php echo 'Hello world'; ?>" > index.php
 
-echo "[信息]正在处理Redis配置..."
-cd /etc/redis/
-wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/redis.conf -O redis.conf
-service redis-server restart
+if [ ${install_redis} = "y" ] ; then
+    echo "[信息]正在安装Redis Server..."
+    apt-get install -y redis-server
+    echo "[信息]正在处理Redis配置..."
+    cd /etc/redis/
+    wget https://raw.githubusercontent.com/anzzyd/docker-automake/master/redis.conf -O redis.conf
+    service redis-server restart
+fi
 
 if [ ${install_swoole} = "y" ] ; then
     echo "[信息]开始安装Swoole扩展..."
@@ -103,6 +108,31 @@ if [ ${install_swoole} = "y" ] ; then
     echo "#Added by build.sh" >> /usr/local/lib/php.ini
     echo "extension=swoole" >> /usr/local/lib/php.ini
 fi
+
+if [ ${load_opcache} = "y" ] ; then
+    echo "#Added by build.sh" >> /usr/local/lib/php.ini
+    echo "zend_extension=opcache" >> /usr/local/lib/php.ini
+    echo "[信息]Zend OPcache已写入php.ini配置，请手动参数开启"
+fi
+
+if [ ${install_redis_extension} = "y" ] ; then
+    echo "[信息]正在下载Redis扩展..."
+    cd /opt
+    wget https://pecl.php.net/get/redis-4.3.0.tgz
+    tar -xvf redis-4.3.0.tgz
+    cd redis-4.3.0
+    echo "[信息]正在编译Redis扩展..."
+    phpize
+    ./configure
+    make && make install
+
+    echo "[信息]Redis扩展安装完成"
+
+    echo "#Added by build.sh" >> /usr/local/lib/php.ini
+    echo "extension=redis" >> /usr/local/lib/php.ini
+    echo "[信息]Redis已加入php.ini"
+fi
+
 
 echo "[信息]正在启动PHP7..."
 /usr/local/sbin/php-fpm
